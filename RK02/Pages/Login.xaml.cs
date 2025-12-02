b2d0451007b9c93afe81a3f87e3dbbca551317bb
basiliac <alyacherkova82@gmail.com>
2025-12-02 16:06:48 +0700

    Added project

---
+++ RK02/Pages/Login.xaml.cs
@@ -0,0 +1,76 @@
+﻿using MovieCatalogApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RK02.Pages
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var db = FakeDatabase.GetContext();
+            // var db = Helper.Get();
+            var adms = db.Users.Where(a => a.Username == tbLogin.Text).ToList();
+
+            if (adms.Count >= 1)
+            {
+                var adm = adms[0];
+                if (adm.Password == tbPassword.Password)
+                {
+                    if (adm.RoleId == 1)
+                    {
+                        int status = 0;
+                        NavigationService.Navigate(new PagesAdmin(status));
+                    }
+                    else if (adm.RoleId == 2)
+                    {
+                        MessageBox.Show(adm.Username);
+                        //NavigationService.Navigate(new MainPage());
+                    }
+                    else
+                        MessageBox.Show("Ваша роль не имеет прав доступа к другим страницам", "Нет доступа", MessageBoxButton.OK, MessageBoxImage.Information);
+
+
+                }
+                else
+                    MessageBox.Show("Неверный пароль", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+                MessageBox.Show("Пользователь не найден", "ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+        }
+
+        private void tbLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_Guest(object sender, RoutedEventArgs e)
+        {
+            int status = 1;
+            NavigationService.Navigate(new PagesAdmin(status));
+        }
+    }
+}
---
+++ RK02/Pages/PagesAdmin.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RK02.Pages
+{
+    /// <summary>
+    /// Interaction logic for PagesAdmin.xaml
+    /// </summary>
+    public partial class PagesAdmin : Page
+    {
+        public PagesAdmin(int status)
+        {
+            if (status == 1)
+            {
+                MessageBox.Show("Добро пожаловать! Вы вошли, как гость");
+            }
+            else
+            {
+                MessageBox.Show("Добро пожаловать!");
+            }
+                InitializeComponent();
+        }
+    }
+}
